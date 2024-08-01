5d325024436092eceac974e70e42f7598f87399a
Monalisa Onyango <+>
2024-08-01 15:41:51 +0300

    understanding the bonus

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ import Header from "./Header"
 import About from "./About"
 import ArticleList from "./ArticleList";
 
-// console.log(blogData);
+console.log(blogData);
 
 function App() {
   return (
-  <div>
+  <div className="App">
        <Header name={blogData.name} />
        <About image={blogData.image} about={blogData.about}/>
        <ArticleList posts={blogData.posts} />
