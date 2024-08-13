d31d3be868f67b57233b281577ad1ef9ab74edee
Monalisa Onyango <+>
2024-08-13 22:37:06 +0300

    counter checking

--- src/components/Article.js
+++ src/components/Article.js
@@ -11,17 +11,14 @@ function makeEmojiList(minutes) {
     return emojis;
   }
   
-
-
-
-function Article({title, date="January 1,1970", preview, minutes}) {
+  function Article({ title, date = "January 1, 1970", preview, minutes }) {
     
     const emojis = makeEmojiList(minutes);
     
     return(
        <article>
             <h3>{title}</h3>
-           <small>{date} {emojis} {minutes} min read</small>
+           <small>{date} • {emojis} {minutes} min read</small>
             <p>{preview}</p>
        </article>
 
